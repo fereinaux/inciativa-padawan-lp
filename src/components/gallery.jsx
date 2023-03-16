cf3337d03eb7de489020b2bd6b20dc70f7a48151
Felipe Reinaux <felipereinaux@gmail.com>
2023-03-16 16:26:55 -0300

    release to the world

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,5 +1,6 @@
 import { Image } from "./image";
 import React from "react";
+import "./style.scss";
 
 export const Gallery = (props) => {
   return (
@@ -13,83 +14,107 @@ export const Gallery = (props) => {
           </p>
         </div>
         <div className="cards">
-          <div className="card-holder">
+          <a
+            className="card-holder"
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Obi Wan*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+          >
             <img src="img/obiwan.png" className="obiwan" alt="" />
             <div className="card-pricing obiwan" id="obiwan">
-              <h6>Obi-Wan</h6>
-              <span className="crossed">R$ 499,90</span>
+              <h6>
+                Obi-Wan <span className="tag-obiwan">Plano Padawan</span>
+              </h6>
+              <span>
+                de <span className="crossed">R$ 499,90</span> por
+              </span>
               <h4>R$ 399,90</h4>
-              <div>
-                <a
-                  target="_blank"
-                  href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Obi Wan*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Adquirir
-                </a>
-              </div>
               <div className="botao">
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Obi Wan*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Adquirir
+                  </a>
+                </div>
                 <p>40 horas de aula</p>
                 <p>Projeto de API para Portfolio</p>
                 <p>Grupo no Whatsapp</p>
+                <p>-</p>
+                <p>-</p>
+                <p>-</p>
               </div>
             </div>
-          </div>
-          <div className="card-holder selected">
+          </a>
+
+          <a
+            className="card-holder selected"
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Darth Vader*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+          >
             <img src="img/vader.png" alt="" className="vader" />
             <div className="card-pricing vader" id="vader">
-              <h6>Darth Vader</h6>
-              <span className="crossed">R$ 599,90</span>
+              <h6>
+                Darth Vader <span className="tag-vader">Plano Jedi</span>
+              </h6>
+              <span>
+                de <span className="crossed">R$ 599,90</span> por
+              </span>
               <h4>R$ 499,90</h4>
-              <div>
-                <a
-                  target="_blank"
-                  href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Darth Vader*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Adquirir
-                </a>
-              </div>
               <div className="botao">
-                <p>40 horas de aula</p>
-                <p>Projeto de API para Portfolio</p>
-                <p>Grupo no Whatsapp</p>
-                <p>
-                  <strong>Simulação de Entrevista</strong>
-                </p>
-                <p>
-                  <strong>Review Linkedin</strong>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="card-holder">
-            <img src="img/yoda.png" alt="" className="yoda" />
-            <div className="card-pricing yoda" id="yoda">
-              <h6>Yoda</h6>
-              <span className="crossed">R$ 799,90</span>
-              <h4>R$ 699,90</h4>
-              <div>
-                <a
-                  target="_blank"
-                  href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Yoda*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Adquirir
-                </a>
-              </div>
-              <div className="botoao">
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Darth Vader*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Adquirir
+                  </a>
+                </div>
                 <p>40 horas de aula</p>
                 <p>Projeto de API para Portfolio</p>
                 <p>Grupo no Whatsapp</p>
                 <p>Simulação de Entrevista</p>
                 <p>Review Linkedin</p>
-                <p>
-                  <strong>Projeto FrontEnd ReactJS</strong>
-                </p>
+                <p>-</p>
               </div>
             </div>
-          </div>
+          </a>
+
+          <a
+            className="card-holder "
+            target="_blank"
+            href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Yoda*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+          >
+            <img src="img/yoda.png" alt="" className="yoda" />
+            <div className="card-pricing yoda" id="yoda">
+              <h6>
+                Yoda <span className="tag-yoda">Plano Mestre</span>
+              </h6>
+              <span>
+                de <span className="crossed">R$ 899,90</span> por
+              </span>
+              <h4>R$ 799,90</h4>
+              <div className="botao">
+                <div>
+                  <a
+                    target="_blank"
+                    href="https://api.whatsapp.com/send?phone=5581997187441&text=Ol%C3%A1,%20me%20interessei%20no%20plano%20*Yoda*%20da%20Iniciativa%20Padawan,%20como%20prosseguir?"
+                    className="btn btn-custom btn-lg page-scroll"
+                  >
+                    Adquirir
+                  </a>
+                </div>
+                <p>60 horas de aula</p>
+                <p>Projeto de API para Portfolio</p>
+                <p>Grupo no Whatsapp</p>
+                <p>Simulação de Entrevista</p>
+                <p>Review Linkedin</p>
+                <p>Projeto FrontEnd ReactJS</p>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
